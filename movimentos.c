581c8b6649f2fbd33e1b72bef92bcce8f61535a9
ricardo <ricardo>
2022-03-17 17:28:59 +0000

    função possivel

--- movimentos.c
+++ movimentos.c
@@ -1,32 +1,70 @@
-void move_rigth(short int grid1[][4], short int vazio[]){
-    if(vazio[1]!=3){                                            // vazio[0] posição do 0 na horizontal       
-        vazio[1]++;                                            //vazio[1] posição do 0 na vertical
-        grid1[vazio[0]][vazio[1]-1]=grid1[vazio[0]][vazio[1]];
-        grid1[vazio[0]][vazio[1]]=0;    
-        
-    }
+void move_rigth(short int grid1[][4],short int x, short int y){    
+    y++;                                            //x posição do 0 na horizontal
+    grid1[x][y-1]=grid1[x][y];                      //y posição do 0 na vertical
+    grid1[x][y]=0;
 }
 
-void move_left(short int grid1[][4], short int vazio[]){
-    if(vazio[1]!=0){                                                  
-        vazio[1]--;                                             
-        grid1[vazio[0]][vazio[1]+1]=grid1[vazio[0]][vazio[1]];   
-        grid1[vazio[0]][vazio[1]]=0;    
-    }
+void move_left(short int grid1[][4],short int x, short int y){                                             
+    y--;                                             
+    grid1[x][y+1]=grid1[x][y];   
+    grid1[x][y]=0;
+
 }
 
-void move_up(short int grid1[][4], short int vazio[]){
-    if(vazio[1]!=0){                                                  
-        vazio[0]--;                                             
-        grid1[vazio[0]+1][vazio[1]]=grid1[vazio[0]][vazio[1]];   
-        grid1[vazio[0]][vazio[1]]=0;    
-    }
+void move_up(short int grid1[][4], short int x, short int y){                                             
+    x--;                                             
+    grid1[x+1][y]=grid1[x][y];   
+    grid1[x][y]=0;    
 }
 
-void move_down(short int grid1[][4], short int vazio[]){
-    if(vazio[1]!=0){                                                  
-        vazio[0]++;                                             
-        grid1[vazio[0]-1][vazio[1]]=grid1[vazio[0]][vazio[1]];   
-        grid1[vazio[0]][vazio[1]]=0;    
+void move_down(short int grid1[][4],short int x, short int y){                                                 
+    x++;                                             
+    grid1[x-1][y]=grid1[x][y];   
+    grid1[x][y]=0;    
+}
+
+bool possivel(short int x, short int y, char move, char last){
+    if(move=='r'){
+        if(last=='l'){
+            return false;
+        }
+        if(x==3){
+            return false;
+        }
+        return true;
     }
+
+
+    else if(move=='l'){
+        if(last=='r'){
+            return false;
+        }
+        if(x==0){
+            return false;
+        }
+        return true;
+    }
+
+
+    else if(move=='u'){
+        if(last=='d'){
+            return false;
+        }
+        if(y==0){
+            return false;
+        }
+        return true;
+    }
+
+
+    else if(move=='d'){
+        if(last=='u'){
+            return false;
+        }
+        if(y==3){
+            return false;
+        }
+        return true;
+    }
+
 }
